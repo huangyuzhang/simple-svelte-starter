874adaf338f5af57ccae6d00e266713da46f8c3f
Simon Huang <huangyuzhang@88.com>
2025-12-11 14:21:37 +0800

    fix header

--- src/lib/components/layouts/header.svelte
+++ src/lib/components/layouts/header.svelte
@@ -1,32 +1,40 @@
 <script lang="ts">
-    import { layout, nav } from "$lib/config";
-	import { cn } from "$lib/utils";
-    import ModeToggle from "$lib/components/mode-toggle.svelte";
-    import Logo from "$lib/components/layouts/logo.svelte";
-    
-    let isScrolled = $state(false);
-    const unScrolledContainerClasses = ["border-transparent"];
-        const scrolledContainerClasses = [
-        "border-neutral-300/50",
-        "bg-white/80",
-        "dark:border-neutral-600/40",
-        "dark:bg-neutral-900/60",
-        "backdrop-blur-2xl",
-        "shadow",
-    ];
+	import { layout, nav } from '$lib/config';
+	import { cn } from '$lib/utils';
+	import ModeToggle from '$lib/components/mode-toggle.svelte';
+	import Logo from '$lib/components/layouts/logo.svelte';
+	import I18nToggle from '../i18n-toggle.svelte';
 
-    const evaluateHeaderPosition = () => {
-        isScrolled = window.scrollY > 16;
-    };
-    
-    $effect(() => {
+	let isScrolled = $state(false);
+
+	const unScrolledContainerClasses = ['border-transparent'];
+	const scrolledContainerClasses = [
+		'border-neutral-300/50',
+		'bg-white/80',
+		'dark:border-neutral-600/40',
+		'dark:bg-neutral-900/60',
+		'backdrop-blur-2xl',
+		'shadow'
+	];
+
+	const evaluateHeaderPosition = () => {
+		// browser only
         if (typeof window !== 'undefined') {
-            window.addEventListener("scroll", evaluateHeaderPosition);
+            isScrolled = window.scrollY > 16;
+        }
+    };
+
+	$effect(() => {
+		// browser only
+        if (typeof window !== 'undefined') {
+			// evaluate header position immediately after the component is mounted
+            evaluateHeaderPosition(); 
+            window.addEventListener('scroll', evaluateHeaderPosition, { passive: true });
         }
 
         return () => {
             if (typeof window !== 'undefined') {
-                window.removeEventListener("scroll", evaluateHeaderPosition);
+                window.removeEventListener('scroll', evaluateHeaderPosition);
             }
         };
     });
@@ -35,39 +43,40 @@
 <!-- a placeholder for reserving space for the header -->
 <div class="w-full h-20 opacity-0 pointer-events-none"></div>
 
-<header 
-    id="header" 
-    class="fixed top-0 z-50 w-full duration-300 ease-out"
-    class:h-20={!isScrolled}
-    class:h-14={isScrolled}
+<header
+	id="header"
+	class="fixed top-0 z-50 w-full duration-300 ease-out"
+	class:h-20={!isScrolled}
+	class:h-14={isScrolled}
 >
-    <div
-        class={cn(
-            layout.headerWidth,
-            "flex items-center justify-between h-full pl-6 pr-4 mx-auto border-b border-l-0 border-r-0 select-none lg:border-r lg:border-l lg:rounded-b-xl duration-300 ease-out",
-            isScrolled ? scrolledContainerClasses.join(' ') : unScrolledContainerClasses.join(' ')
-        )}
-    >
-        <Logo />
+	<div
+		class={cn(
+			layout.headerWidth,
+			'flex items-center justify-between h-full pl-6 pr-4 mx-auto border-b border-l-0 border-r-0 select-none lg:border-r lg:border-l lg:rounded-b-xl duration-300 ease-out',
+			isScrolled ? scrolledContainerClasses.join(' ') : unScrolledContainerClasses.join(' ')
+		)}
+	>
+		<Logo />
 
-        <nav
-        class="relative z-30 flex flex-row-reverse justify-start w-full text-sm sm:justify-end text-neutral-500 dark:text-neutral-400 sm:flex-row"
-        >
-            <div
-                id="menu"
-                class="flex flex-row items-center justify-end h-auto w-auto text-sm sm:text-sm sm:w-auto"
-            >
-                {#each nav.main as menu}
-                        <a
-                        href={menu.url}
-                        class="relative flex items-center justify-center px-3 py-2 font-medium tracking-wide text-center duration-200 ease-out sm:py-0 hover:text-neutral-900 dark:hover:text-white"
-                        >
-                        {menu.name}
-                        </a>
-                {/each}
-            </div>
-            
-            <ModeToggle />
-        </nav>
-    </div>
+		<nav
+			class="relative z-30 flex flex-row-reverse justify-start w-full text-sm sm:justify-end text-neutral-500 dark:text-neutral-400 sm:flex-row"
+		>
+			<div
+				id="menu"
+				class="flex flex-row items-center justify-end h-auto w-auto text-sm sm:text-sm sm:w-auto"
+			>
+				{#each nav.main as menu}
+					<a
+						href={menu.url}
+						class="relative flex items-center justify-center px-3 py-2 font-medium tracking-wide text-center duration-200 ease-out sm:py-0 hover:text-neutral-900 dark:hover:text-white"
+					>
+						{menu.name}
+					</a>
+				{/each}
+			</div>
+
+			<I18nToggle />
+			<ModeToggle />
+		</nav>
+	</div>
 </header>
