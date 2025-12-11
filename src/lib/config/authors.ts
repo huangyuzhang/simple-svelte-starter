54b7346c4357f04d2a50871c7271db6c5fde93c5
Simon Huang <huangyuzhang@88.com>
2025-12-11 17:23:04 +0800

    add `routes/authors`

---
+++ src/lib/config/authors.ts
@@ -0,0 +1,10 @@
+export const authors = {
+    "simon": {
+        name: "Simon Huang",
+        bio: "Frontend Developer & UI/UX Enthusiast",
+        avatar: "https://github.com/huangyuzhang.png",
+        github: "https://github.com/huangyuzhang",
+    },
+};
+
+export type Author = keyof typeof authors;
---
+++ src/routes/(collections)/authors/[author]/+page.ts
@@ -0,0 +1,26 @@
+import { authors } from "$lib/config/authors";
+import { error } from "@sveltejs/kit";
+
+export async function load({ params }) {
+    const posts = import.meta.glob("/src/collections/posts/*.md", { eager: true });
+    const authorSlug = params.author;
+    const author = authors[authorSlug];
+
+    if (!author) {
+        throw error(404, `Author ${authorSlug} not found`);
+    }
+
+    const authorPosts = Object.entries(posts)
+        .map(([path, file]) => {
+            const slug = path.split("/").at(-1)?.replace(".md", "");
+            const metadata = file.metadata;
+            return { slug, ...metadata };
+        })
+        .filter((post) => post.author === authorSlug)
+        .sort((a, b) => new Date(b.date).getTime() - new Date(a.date).getTime());
+
+    return {
+        author,
+        posts: authorPosts,
+    };
+}
